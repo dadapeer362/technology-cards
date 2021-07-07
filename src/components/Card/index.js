0726a58e06f8bc167aff7bb17b015acec0ce229f
Shaik Dadapeer <shaikdadapeer362@gmail.com>
2021-07-07 11:15:12 +0000

    commit message

---
+++ src/components/Card/index.js
@@ -0,0 +1,20 @@
+// Write your code here.
+import './index.css'
+
+const Card = props => {
+  const {cardData} = props
+  const {id, title, description, imgUrl, className} = cardData
+  return (
+    <div className={className}>
+      <div className="inner-container">
+        <h1 className="card-heading">{title}</h1>
+        <p className="card-paragraph">{description}</p>
+        <div className="img-style">
+          <img src={imgUrl} alt="img" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Card
